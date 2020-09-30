535763982ab2a51275004cdbac109e33e1e29bbb
binbin0915 <184821967@qq.com>
2020-09-30 11:13:11 +0800

    swipe refresh

--- src/JetpackDemo/app/src/main/java/cz/yb/jetpackdemo/ui/PagingActivity.java
+++ src/JetpackDemo/app/src/main/java/cz/yb/jetpackdemo/ui/PagingActivity.java
@@ -10,6 +10,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.scwang.smart.refresh.footer.ClassicsFooter;
+import com.scwang.smart.refresh.header.ClassicsHeader;
+import com.scwang.smart.refresh.layout.api.RefreshLayout;
+import com.scwang.smart.refresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smart.refresh.layout.listener.OnRefreshListener;
+
 import cz.yb.jetpackdemo.R;
 import cz.yb.jetpackdemo.adapter.PagingAdapter;
 import cz.yb.jetpackdemo.entity.WorksEntity;
@@ -31,6 +37,22 @@ public class PagingActivity extends AppCompatActivity {
             @Override
             public void onChanged(PagedList<WorksEntity> worksEntities) {
                 adapter.submitList(worksEntities);
+            }
+        });
+
+        RefreshLayout refreshLayout = (RefreshLayout)findViewById(R.id.refreshLayout);
+        refreshLayout.setRefreshHeader(new ClassicsHeader(this));
+        refreshLayout.setRefreshFooter(new ClassicsFooter(this));
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                refreshlayout.finishRefresh(2000/*,false*/);//传入false表示刷新失败
+            }
+        });
+        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(RefreshLayout refreshlayout) {
+                refreshlayout.finishLoadMore(2000/*,false*/);//传入false表示加载失败
             }
         });
 
